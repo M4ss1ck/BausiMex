aa6cc3d332d06bd216b3747b9c10c54cf19c11c0
Andy <4ndyraul@gmail.com>
2021-04-16 20:29:31 -0400

    Fixed parallax

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,80 +4,88 @@ import tropical from '../images/travelpalmtreesymbol.svg'
 import edificio from '../images/buildng.svg'
 import Plx from 'react-plx'
 
+import useWindowDimensions from '../hooks/useWindowDimension'
+
 const Inicio = () => {
+
+  const { height } = useWindowDimensions();
+  
     const parallaxData = [
         {
           start: 0,
-          end: 300,
+          end: height,
           properties: [
             {
               startValue: 1,
-              endValue: 0.1,
+              endValue: 0,
               property: 'opacity',
             },
           ],
         },
-        {
-            start: 300,
-            end: 500,
-            properties: [
-              {
-                startValue: 0.1,
-                endValue: 1,
-                property: 'opacity',
-              },
-              {
-                startValue: "#3cb99c",
-                endValue: "rgba(50,50,200,0.8)",
-                property: "backgroundColor"
-              }
-            ],
-          },
-        {
-            start: 250,
-            end: 500,
-            properties: [
-              {
-                startValue: "rgba(50,50,200,0.8)",
-                endValue: "transparent",
-                property: "backgroundColor"
-              },
-            ],
-          }
+        // {
+        //     start: 300,
+        //     end: 500,
+        //     properties: [
+        //       {
+        //         startValue: 0.1,
+        //         endValue: 1,
+        //         property: 'opacity',
+        //       },
+        //       {
+        //         startValue: "#3cb99c",
+        //         endValue: "rgba(50,50,200,0.8)",
+        //         property: "backgroundColor"
+        //       }
+        //     ],
+        //   },
+        // {
+        //     start: 250,
+        //     end: 500,
+        //     properties: [
+        //       {
+        //         startValue: "rgba(50,50,200,0.8)",
+        //         endValue: "#FFF",
+        //         property: "backgroundColor"
+        //       },
+        //     ],
+        //   }
       ];
     
     return (
-        <Plx
-        className='MyAwesomeParallax'
-        parallaxData={ parallaxData } // your parallax effects, see beneath
-      >
-        
-            <Presentacion id="top"/>
-            <h1 >Página principal</h1>
-            <br/>
-            <p>Algo de texto</p>
-            <figure>
-                <img
-                src={tropical}
-                alt="playa y palmeras"
-                width="300"
-                />
-                <figcaption>Una imagen para relajarse</figcaption>
-            </figure>
-            <p>Opiniones de algunos usuarios (Todas muy favorables)</p>
-            <p>Alguna tabla, formulario o algo así (o nada de eso)</p>
-            <figure>
-                <img
-                src={edificio}
-                alt="edificio"
-                width="500"
-                />
-                <figcaption>Otra imagen. Esta vez un edificio</figcaption>
-            </figure>
-            <div className="btn-subir">
-              <a href="#top">Ir al inicio</a>
-            </div>
-      </Plx>
+        <div className="otra-cosa">
+          <Plx
+            className='MyAwesomeParallax'
+            parallaxData={ parallaxData } // your parallax effects, see beneath
+            >
+          
+              <Presentacion id="top"/>
+
+          </Plx>
+              <h1 >Página principal </h1>
+              <br/>
+              <p>Algo de texto</p>
+              <figure>
+                  <img
+                  src={tropical}
+                  alt="playa y palmeras"
+                  width="300"
+                  />
+                  <figcaption>Una imagen para relajarse</figcaption>
+              </figure>
+              <p>Opiniones de algunos usuarios (Todas muy favorables)</p>
+              <p>Alguna tabla, formulario o algo así (o nada de eso)</p>
+              <figure>
+                  <img
+                  src={edificio}
+                  alt="edificio"
+                  width="500"
+                  />
+                  <figcaption>Otra imagen. Esta vez un edificio</figcaption>
+              </figure>
+              <div className="btn-subir">
+                <a href="#top">Ir al inicio</a>
+              </div>
+        </div>
         
     )
 }

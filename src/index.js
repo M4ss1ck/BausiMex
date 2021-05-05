a1273988eaeb40e0507236b894efda6cff683116
Andy <4ndyraul@gmail.com>
2021-05-05 16:05:44 -0400

    react-snapshot added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './bootstrap.min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-snapshot";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-

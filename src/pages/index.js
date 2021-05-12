aa565ca1f8403cdfce508a44159ca797b17511d7
Andy <4ndyraul@gmail.com>
2021-05-11 18:37:07 -0700

    moved to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Contacto from "./components/Contacto";
-import Diseno from "./components/Diseno";
-import Fabricacion from "./components/Fabricacion";
-import Galeria from "./components/Galeria";
-import Inicio from "./components/Inicio";
-import Montaje from "./components/Montaje";
-import Footer from "./components/more/Footer";
-import Navegacion from "./components/Navegacion";
+import Contacto from "../components/Contacto";
+import Diseno from "../components/Diseno";
+import Fabricacion from "../components/Fabricacion";
+import Galeria from "../components/Galeria";
+import Inicio from "../components/Inicio";
+import Montaje from "../components/Montaje";
+import Footer from "../components/more/Footer";
+import Navegacion from "../components/Navegacion";
+
+import "../layout/bootstrap.min.css";
+import "../layout/index.css";
+
 
 function App() {
   return (
